682fdc2295c1400f21ad1112ff9534122486b6a4
tengfei,liang <44048948+tyutltf@users.noreply.github.com>
2019-05-04 19:52:05 +0800

    优化代码

--- bluelog/settings.py
+++ bluelog/settings.py
@@ -22,12 +22,16 @@ class BaseConfig(object):
     CKEDITOR_ENABLE_CSRF = True
     CKEDITOR_FILE_UPLOADER = 'admin.upload_image'
 
-    MAIL_SERVER = os.getenv('MAIL_SERVER')
+
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = ('Bluelog Admin', MAIL_USERNAME)
+    # MAIL_SERVER = os.getenv('MAIL_SERVER')
+    # MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_USERNAME = '18235121656@163.com'
+    MAIL_PASSWORD = 'ltf1997'
+    MAIL_DEFAULT_SENDER = ('Bluelog Admin ltf', MAIL_USERNAME)
 
     BLUELOG_EMAIL = os.getenv('BLUELOG_EMAIL')
     BLUELOG_POST_PER_PAGE = 10
--- tests/base.py
+++ tests/base.py
@@ -18,7 +18,7 @@ class BaseTestCase(unittest.TestCase):
         self.runner = app.test_cli_runner()
 
         db.create_all()
-        user = Admin(name='ltf', username='grey', about='I am test', blog_title='Testlog', blog_sub_title='a test')
+        user = Admin(name='ltf', username='lsq', about='I am test', blog_title='Testlog', blog_sub_title='a test')
         user.set_password('123')
         db.session.add(user)
         db.session.commit()
